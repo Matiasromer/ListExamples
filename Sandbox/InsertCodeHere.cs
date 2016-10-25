6923702f5d0290821728380a53aefe4471851505
Matias Rømer <Matias Rmer@MATIAS-NB>
2016-10-25 11:23:47 +0200

    Alle case til 7. 7 findes gennemsnittet og 6 fås summen

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -15,21 +15,21 @@ namespace Sandbox
             aListOfInt.Add(9);
 
             // Case 1
-            // Console.WriteLine("Case 1: Element with index 1 is {0}", aListOfInt[1]);
+             Console.WriteLine("Case 1: Element with index 1 is {0}", aListOfInt[1]);
 
             // Case 2
-            // Console.WriteLine("Case 2: List contains {0} elements", aListOfInt.Count);
+             Console.WriteLine("Case 2: List contains {0} elements", aListOfInt.Count);
  
             aListOfInt.Add(5);
             aListOfInt.Add(22);
 
             // Case 3
-            // Console.WriteLine("Case 3: Element with index 3 is {0}", aListOfInt[3]);
+             Console.WriteLine("Case 3: Element with index 3 is {0}", aListOfInt[3]);
 
             aListOfInt.RemoveAt(0);
 
             // Case 4
-            // Console.WriteLine("Case 4: Element with index 3 is {0}", aListOfInt[3]);
+             Console.WriteLine("Case 4: Element with index 3 is {0}", aListOfInt[3]);
 
 
 
@@ -43,19 +43,38 @@ namespace Sandbox
 
             // Case 5: Make some code that prints out 
             // all the elements in the list
+            foreach (int l in aListOfInt)
+            {
+                Console.WriteLine($"Tallene er : {l}");
+            }
 
 
 
             // Case 6: Make some code that finds the 
             // sum of the elements in the list, and prints the result
 
-
+            int code = 0;
+            foreach(int list in aListOfInt)
+            {
+                code = code + list;
+            }
+            Console.WriteLine($"damn son, summen af lortet er sku {code}");
 
             // Case 7: Make some code that finds the 
             // average of the elements in the list, and prints the result
             // Tip: Think about how average is defined; then
             // you might reuse something from Case 6
 
+            int gennemsnit = 0;
+            int count = 0;
+            foreach (int lis in aListOfInt)
+            {
+                gennemsnit = gennemsnit + lis;
+                count++;
+            }
+            Console.WriteLine($"Average shit er : {gennemsnit/count}" );
+
+            
 
 
             // [DIFFICULT]
